44d5e81010995919f2c846b3a8317f565ac474dc
Adam Baker <adam.baker97202@gmail.com>
2016-10-28 16:33:07 -0700

    Ping Pong Week 3
    display ping pong if the number entered is divisible by 15, pong if it
    is divisible by 5, ping if it is divisible by 3, and just display the
    number if it does not fulfill any of these requirements.  The program
    is able to count into the thousands before the number is finally too
    big and crashes (when using the loops/ways understood/known by myself).

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
+//back
+function pingPong(userInput){
+var result = [];
+for (var i = 1; i <= userInput; i++){
+  if ((i % 15) === 0){
+    result.push ("Ping-Pong!");
+  } else if ((i % 5) === 0){
+    result.push ("Pong!");
+  } else if ((i % 3) === 0){
+    result.push ("Ping!");
+  } else
+    result.push (i);
+}
+return result;
+}
+
+//front
 $(document).ready(function() {
-
-        function ping() {
-          input = $("#ping");
-
-          input.empty();
-
-        var num =
+  $("form#formPing").submit(function(event) {
+    $("#userAnswer").empty();
+    var userInput = parseInt($("input#number").val());
+    var result = pingPong(userInput);
+    result.forEach(function(output) {
+    $("#userAnswer").append("<li>" + output + "</li>");
+    event.preventDefault();
+  });
+});
+});
